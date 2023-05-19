dcc431d73469e9bd6f39b06933162aeae13f61ba
Aztarok <Aztarok2994@gmail.com>
2023-05-18 22:59:58 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="h-screen w-screen flex items-center justify-center">
+                <h1 className="text-3xl font-bold bg-blue-600 underline">
+                    Hello world!
+                </h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
